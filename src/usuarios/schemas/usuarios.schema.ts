63596b6b4be4efcf6923170ae3ce6c6db347ece7
endymacea <83047587+endymacea@users.noreply.github.com>
2025-05-27 17:38:41 -0500

    Update usuarios.schema.ts

--- src/usuarios/schemas/usuarios.schema.ts
+++ src/usuarios/schemas/usuarios.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import mongoose from 'mongoose';
 import { Document } from 'mongoose';
 
-@Schema({
+@Schema( {
     timestamps: true
 })
 
